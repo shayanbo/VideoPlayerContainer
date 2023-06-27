2370fc5d633f56424dff8247859ef242311862d7
rock <shayanbo@bilibili.com>
2023-06-27 09:13:02 +0800

    feat:save

--- VideoPlayerContainer/Overlayer/RenderWidget.swift
+++ VideoPlayerContainer/Overlayer/RenderWidget.swift
@@ -7,8 +7,12 @@
 
 import Foundation
 import SwiftUI
-import UIKit
 import AVKit
+#if os(iOS) || os(watchOS) || os(tvOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
 
 public class RenderService : Service {
     
@@ -57,6 +61,8 @@ struct RenderWidget : View {
     }
 }
 
+#if os(iOS) || os(watchOS) || os(tvOS)
+
 struct RenderView : UIViewRepresentable {
 
     let player: AVPlayer
@@ -98,3 +104,56 @@ class PlayerView: UIView {
         return AVPlayerLayer.self
     }
 }
+
+#elseif os(macOS)
+
+struct RenderView : NSViewRepresentable {
+    
+    let player: AVPlayer
+    let gravity: AVLayerVideoGravity
+    
+    func makeNSView(context: NSViewRepresentableContext<Self>) -> PlayerView {
+        let playerView = PlayerView()
+        playerView.player = player
+        playerView.videoGravity = gravity
+        return playerView
+    }
+
+    func updateNSView(_ uiView: PlayerView, context: NSViewRepresentableContext<Self>) {
+        uiView.videoGravity = gravity
+    }
+}
+
+class PlayerView: NSView {
+    
+    let renderCanvas = AVPlayerLayer()
+    
+    init() {
+        super.init(frame: .zero)
+        wantsLayer = true
+        renderCanvas.frame = bounds
+        renderCanvas.autoresizingMask = [.layerHeightSizable, .layerWidthSizable]
+        layer?.addSublayer(renderCanvas)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var videoGravity: AVLayerVideoGravity {
+        get {
+            return renderCanvas.videoGravity
+        }
+        set {
+            renderCanvas.videoGravity = newValue
+        }
+    }
+    
+    var player: AVPlayer? {
+        didSet {
+            renderCanvas.player = player
+        }
+    }
+}
+
+#endif
---
+++ macOS-Example/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  VideoPlayer
+//
+//  Created by shayanbo on 2023/6/13.
+//
+
+import SwiftUI
+import AVKit
+import VideoPlayerContainer
+
+let wwdcVideo = "https://devstreaming-cdn.apple.com/videos/wwdc/2023/10036/4/BB960BFD-F982-4800-8060-5674B049AC5A/cmaf/hvc/2160p_16800/hvc_2160p_16800.m3u8"
+
+struct ContentView: View {
+    
+    @StateObject var context = Context()
+    
+    var body: some View {
+        
+        PlayerWidget()
+            .ignoresSafeArea(edges: .vertical)
+            .frame(maxWidth: .infinity, maxHeight: 300)
+            .bindContext(context, launch: [
+                LoadingService.self,
+            ])
+            .onAppear {
+                
+                let controlService = context[ControlService.self]
+                
+                /// halfScreen top
+                controlService.configure(.halfScreen(.top)) {[
+                    IdentifableView(id: "back") { BackButtonWidget() },
+                    IdentifableView(id: "title") { TitleWidget() },
+                    IdentifableView(id: "space") { Spacer() },
+                    IdentifableView(id: "playback") {  Button("Hello World") {
+                        context[FeatureService.self].present(.right(.squeeze)) {AnyView(
+                            Form {
+                                Text("World")
+                                Text("World")
+                                Text("World")
+                            }.frame(width: 100)
+                        )}
+                    } },
+                    IdentifableView(id: "more") {  MoreButtonWidget() }
+                ]}
+                
+                /// halfScreen bottom
+                controlService.configure(.halfScreen(.bottom)) {[
+                    IdentifableView(id: "playback") {  PlaybackButtonWidget() },
+                    IdentifableView(id: "progress") {  ProgressWidget()   },
+                    IdentifableView(id: "timeline") {  TimelineWidget()   }
+                ]}
+                
+                /// halfScreen center
+                controlService.configure(.halfScreen(.center)) {[
+                    IdentifableView(id: "playback") {  Button("Hello World") {
+                        context[FeatureService.self].present(.left(.cover)) {AnyView(
+                            Form {
+                                Text("Hello")
+                                Text("Hello")
+                                Text("Hello")
+                            }.frame(width: 100)
+                        )}
+                    } },
+                ]}
+                
+                // configure title
+                context[TitleService.self].setTitle("WWDC Video")
+                
+                // load video item
+                let item = AVPlayerItem(url: URL(string:wwdcVideo)!)
+                context[RenderService.self].player.replaceCurrentItem(with: item)
+                
+                // configure toast view
+                context[ToastService.self].configure { toast in
+                    Text(toast.title)
+                }
+                
+                // configure more widget
+                context[MoreButtonService.self].bindClickHandler { [weak context] in
+                    context?[ToastService.self].toast(ToastService.Toast(title: "Hahahahaha"))
+                }
+                
+                // configure control style
+                context[ControlService.self].configure(controlStyle: .manual)
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
