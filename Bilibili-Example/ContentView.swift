55c903c6e33887e65c3e2ad2afa42cfd264b3547
rock <shayanbo@bilibili.com>
2023-07-04 09:22:50 +0800

    feat:save

--- Bilibili-Example/ContentView.swift
+++ Bilibili-Example/ContentView.swift
@@ -17,104 +17,112 @@ struct ContentView: View {
     
     var body: some View {
         
-        PlayerWidget()
-            .ignoresSafeArea(.all)
-            .frame(maxHeight: orientation.isLandscape ? .infinity : 300)
-            .bindContext(context)
-            .background(.black)
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification), perform: { _ in
-                self.orientation = UIDevice.current.orientation
-                
-                if UIDevice.current.orientation.isLandscape {
-                    context[StatusService.self].toFullScreen()
-                } else {
-                    context[StatusService.self].toHalfScreen()
-                }
-            })
-            .onAppear {
-                
-                let controlService = context[ControlService.self]
-                
-                controlService.configure(.halfScreen(.bottom), transition: .opacity)
-                controlService.configure(.halfScreen(.top), transition: .opacity)
-                
-                controlService.configure(.halfScreen, insets: .init(top: 10, leading: 5, bottom: 10, trailing: 5))
-                
-                controlService.configure(.halfScreen(.top1)) {[
-                    Image(systemName: "chevron.left").foregroundColor(.white).frame(width: 30, height: 30),
-                    Spacer(),
-                    Image(systemName: "music.quarternote.3").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "airplayvideo").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "ellipsis").foregroundColor(.white).frame(width: 30, height: 30).rotationEffect(.degrees(90)),
-                ]}
-                
-                controlService.configure(.halfScreen(.bottom1)) {[
-                    PlaybackWidget(),
-                    SeekBarWidget(),
-                    TimelineWidget(),
-                    Image(systemName: "arrow.up.left.and.arrow.down.right").frame(width: 30, height: 30).foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.fullScreen, insets: .init(top: 0, leading: 60, bottom: 34, trailing: 60))
-                
-                controlService.configure(.fullScreen(.bottom), transition: .opacity)
-                controlService.configure(.fullScreen(.top), transition: .opacity)
-                controlService.configure(.fullScreen(.left), transition: .opacity)
-                controlService.configure(.fullScreen(.right), transition: .opacity)
-                
-                controlService.configure(.fullScreen(.top1)) {[
-                    Spacer(),
-                    Text("12:12").foregroundColor(.white),
-                    Spacer(),
-                    Image(systemName: "wifi").foregroundColor(.white),
-                    Image(systemName: "battery.75").foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.fullScreen(.top2)) {[
-                    Image(systemName: "chevron.left").foregroundColor(.white).frame(width: 30, height: 30),
-                    Text("Michael Jackson - Thriller (Official 4K Video)").foregroundColor(.white),
-                    Spacer(),
-                    Image(systemName: "hand.thumbsup").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "hand.thumbsdown").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "bitcoinsign.circle").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "battery.100.bolt").foregroundColor(.white).frame(width: 30, height: 30).rotationEffect(.degrees(90)),
-                    Image(systemName: "arrowshape.turn.up.right").foregroundColor(.white).frame(width: 30, height: 30),Image(systemName: "ellipsis").foregroundColor(.white).frame(width: 30, height: 30).rotationEffect(.degrees(90)),
-                ]}
-                
-                controlService.configure(.fullScreen(.bottom1)) {[
-                    PlaybackWidget(),
-                    Image(systemName: "heart.text.square.fill").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "square.text.square.fill").foregroundColor(.white).frame(width: 30, height: 30),
-                    DanmakuWidget(),
-                    Text("字幕").font(.subheadline).foregroundColor(.white),
-                    Text("倍速").font(.subheadline).foregroundColor(.white),
-                    Text("自动").font(.subheadline).foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.fullScreen(.bottom2)) {[
-                    TimelineWidget(),
-                    SeekBarWidget(),
-                    DurationWidget(),
-                ]}
-                
-                controlService.configure(.fullScreen(.right)) {[
-                    Image(systemName: "gift.fill").foregroundColor(.white).frame(width: 30, height: 30),
-                    Image(systemName: "camera.fill").foregroundColor(.white).frame(width: 30, height: 30),
-                ]}
-                
-                controlService.configure(.fullScreen(.left)) {[
-                    Spacer().frame(height: 10),
-                    Button("+关注") {}
-                        .foregroundColor(.white)
-                        .cornerRadius(3)
-                        .buttonStyle(.borderedProminent),
-                    Spacer(),
-                ]}
-                
-                let item = AVPlayerItem(url: Bundle.main.url(forResource: "demo", withExtension: "mp4")!)
-                context[RenderService.self].player.replaceCurrentItem(with: item)
-                context[RenderService.self].player.play()
+        VStack {
+            
+            Spacer().frame(height: orientation.isLandscape ? 0 : 60)
+            
+            GeometryReader { proxy in
+                PlayerWidget()
+                    .frame(maxHeight: orientation.isLandscape ? .infinity : proxy.size.width * 0.5625)
+                    .bindContext(context)
+                    .background(.black)
+                    .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification), perform: { _ in
+                        self.orientation = UIDevice.current.orientation
+                        
+                        if UIDevice.current.orientation.isLandscape {
+                            context[StatusService.self].toFullScreen()
+                        } else {
+                            context[StatusService.self].toHalfScreen()
+                        }
+                    })
+                    .onAppear {
+                        
+                        let controlService = context[ControlService.self]
+                        
+                        controlService.configure(.halfScreen(.bottom), transition: .opacity)
+                        controlService.configure(.halfScreen(.top), transition: .opacity)
+                        
+                        controlService.configure(.halfScreen, insets: .init(top: 10, leading: 5, bottom: 10, trailing: 5))
+                        
+                        controlService.configure(.halfScreen(.top1)) {[
+                            Image(systemName: "chevron.left").foregroundColor(.white).frame(width: 30, height: 30),
+                            Spacer(),
+                            Image(systemName: "music.quarternote.3").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "airplayvideo").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "ellipsis").foregroundColor(.white).frame(width: 30, height: 30).rotationEffect(.degrees(90)),
+                        ]}
+                        
+                        controlService.configure(.halfScreen(.bottom1)) {[
+                            PlaybackWidget(),
+                            SeekBarWidget(),
+                            TimelineWidget(),
+                            Image(systemName: "arrow.up.left.and.arrow.down.right").frame(width: 30, height: 30).foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.fullScreen, insets: .init(top: 0, leading: 60, bottom: 34, trailing: 60))
+                        
+                        controlService.configure(.fullScreen(.bottom), transition: .opacity)
+                        controlService.configure(.fullScreen(.top), transition: .opacity)
+                        controlService.configure(.fullScreen(.left), transition: .opacity)
+                        controlService.configure(.fullScreen(.right), transition: .opacity)
+                        
+                        controlService.configure(.fullScreen(.top1)) {[
+                            Spacer(),
+                            Text("12:12").foregroundColor(.white),
+                            Spacer(),
+                            Image(systemName: "wifi").foregroundColor(.white),
+                            Image(systemName: "battery.75").foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.top2)) {[
+                            Image(systemName: "chevron.left").foregroundColor(.white).frame(width: 30, height: 30),
+                            Text("Michael Jackson - Thriller (Official 4K Video)").foregroundColor(.white),
+                            Spacer(),
+                            Image(systemName: "hand.thumbsup").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "hand.thumbsdown").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "bitcoinsign.circle").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "battery.100.bolt").foregroundColor(.white).frame(width: 30, height: 30).rotationEffect(.degrees(90)),
+                            Image(systemName: "arrowshape.turn.up.right").foregroundColor(.white).frame(width: 30, height: 30),Image(systemName: "ellipsis").foregroundColor(.white).frame(width: 30, height: 30).rotationEffect(.degrees(90)),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.bottom1)) {[
+                            PlaybackWidget(),
+                            Image(systemName: "heart.text.square.fill").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "square.text.square.fill").foregroundColor(.white).frame(width: 30, height: 30),
+                            DanmakuWidget(),
+                            Text("字幕").font(.subheadline).foregroundColor(.white),
+                            Text("倍速").font(.subheadline).foregroundColor(.white),
+                            Text("自动").font(.subheadline).foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.bottom2)) {[
+                            TimelineWidget(),
+                            SeekBarWidget(),
+                            DurationWidget(),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.right)) {[
+                            Image(systemName: "gift.fill").foregroundColor(.white).frame(width: 30, height: 30),
+                            Image(systemName: "camera.fill").foregroundColor(.white).frame(width: 30, height: 30),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.left)) {[
+                            Spacer().frame(height: 10),
+                            Button("+关注") {}
+                                .foregroundColor(.white)
+                                .cornerRadius(3)
+                                .buttonStyle(.borderedProminent),
+                            Spacer(),
+                        ]}
+                        
+                        let item = AVPlayerItem(url: Bundle.main.url(forResource: "demo", withExtension: "mp4")!)
+                        context[RenderService.self].player.replaceCurrentItem(with: item)
+                        context[RenderService.self].player.play()
+                    }
             }
+            Spacer()
+        }
+        .ignoresSafeArea(edges: .all)
     }
 }
 
--- Youtube-Example/ContentView.swift
+++ Youtube-Example/ContentView.swift
@@ -19,129 +19,137 @@ struct ContentView: View {
     
     var body: some View {
         
-        PlayerWidget()
-            .frame(maxHeight: orientation.isLandscape ? .infinity : 300)
-            .bindContext(context)
-            .ignoresSafeArea(edges: .all)
-            .background(.black)
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification), perform: { _ in
-                self.orientation = UIDevice.current.orientation
-                
-                if UIDevice.current.orientation.isLandscape {
-                    context[StatusService.self].toFullScreen()
-                } else {
-                    context[StatusService.self].toHalfScreen()
-                }
-            })
-            .onAppear {
-                
-                let controlService = context[ControlService.self]
-                
-                controlService.configure(.halfScreen(.top), shadow: nil)
-                controlService.configure(.halfScreen(.bottom), shadow: nil)
-                controlService.configure(.fullScreen(.top), shadow: nil)
-                controlService.configure(.fullScreen(.bottom), shadow: nil)
-                controlService.configure(shadow: AnyView(
-                    Rectangle().fill(.black.opacity(0.2)).allowsHitTesting(false)
-                ))
-                
-                controlService.configure(.halfScreen(.top), transition: .opacity)
-                controlService.configure(.halfScreen(.bottom), transition: .opacity)
-                
-                controlService.configure(.halfScreen, insets: .init(top: 0, leading: 10, bottom: 0, trailing: 10))
-                
-                controlService.configure(.halfScreen(.top1)) {[
-                    Image(systemName: "chevron.down").frame(width: 25, height: 35).foregroundColor(.white),
-                    Spacer(),
-                    Image(systemName: "switch.2").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "airplayvideo").frame(width: 25, height: 35).foregroundColor(.white),
-                    Spacer().frame(width: 50),
-                    Image(systemName: "gearshape").frame(width: 25, height: 35).foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.halfScreen(.bottom1)) {[
-                    SeekBarWidget().padding(.horizontal, -20)
-                ]}
-                
-                controlService.configure(.halfScreen(.bottom2)) {[
-                    TimelineWidget(),
-                    Spacer(),
-                    Image(systemName: "square.on.square").frame(width: 25, height: 25).foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.halfScreen(.center)) { views in
-                    HStack(spacing: 30) {
-                        ForEach(views) { $0 }
-                    }
-                }
-                
-                controlService.configure(.halfScreen(.center)) {[
-                    StepBackWidget(),
-                    PlaybackWidget(),
-                    StepForwardWidget(),
-                ]}
-                
-                controlService.configure(.fullScreen(.top1)) {[
-                    Text("Michael Jackson - Thriller (Official 4K Video)").lineLimit(1).foregroundColor(.white),
-                    Spacer(),
-                    Toggle("", isOn: $toggle).fixedSize().frame(width: 50),
-                    Image(systemName: "airplayvideo").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "captions.bubble").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "gearshape").frame(width: 25, height: 35).foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.fullScreen(.bottom1)) {[
-                    Image(systemName: "hand.thumbsup").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "hand.thumbsdown").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "ellipsis.message")
-                        .frame(width: 25, height: 35).foregroundColor(.white)
-                        .allowsHitTesting(true)
-                        .onTapGesture {
-                            context[FeatureService.self].present(.right(.squeeze(0))) {
-                                AnyView(CommentWidget())
+        VStack {
+            
+            Spacer().frame(height: orientation.isLandscape ? 0 : 60)
+            
+            GeometryReader { proxy in
+                PlayerWidget()
+                    .frame(maxHeight: orientation.isLandscape ? .infinity : proxy.size.width * 0.5625)
+                    .bindContext(context)
+                    .background(.black)
+                    .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification), perform: { _ in
+                        self.orientation = UIDevice.current.orientation
+                        
+                        if UIDevice.current.orientation.isLandscape {
+                            context[StatusService.self].toFullScreen()
+                        } else {
+                            context[StatusService.self].toHalfScreen()
+                        }
+                    })
+                    .onAppear {
+                        
+                        let controlService = context[ControlService.self]
+                        
+                        controlService.configure(.halfScreen(.top), shadow: nil)
+                        controlService.configure(.halfScreen(.bottom), shadow: nil)
+                        controlService.configure(.fullScreen(.top), shadow: nil)
+                        controlService.configure(.fullScreen(.bottom), shadow: nil)
+                        controlService.configure(shadow: AnyView(
+                            Rectangle().fill(.black.opacity(0.2)).allowsHitTesting(false)
+                        ))
+                        
+                        controlService.configure(.halfScreen(.top), transition: .opacity)
+                        controlService.configure(.halfScreen(.bottom), transition: .opacity)
+                        
+                        controlService.configure(.halfScreen, insets: .init(top: 0, leading: 10, bottom: 0, trailing: 10))
+                        
+                        controlService.configure(.halfScreen(.top1)) {[
+                            Image(systemName: "chevron.down").frame(width: 25, height: 35).foregroundColor(.white),
+                            Spacer(),
+                            Image(systemName: "switch.2").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "airplayvideo").frame(width: 25, height: 35).foregroundColor(.white),
+                            Spacer().frame(width: 50),
+                            Image(systemName: "gearshape").frame(width: 25, height: 35).foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.halfScreen(.bottom1)) {[
+                            SeekBarWidget().padding(.horizontal, -20)
+                        ]}
+                        
+                        controlService.configure(.halfScreen(.bottom2)) {[
+                            TimelineWidget(),
+                            Spacer(),
+                            Image(systemName: "square.on.square").frame(width: 25, height: 25).foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.halfScreen(.center)) { views in
+                            HStack(spacing: 30) {
+                                ForEach(views) { $0 }
                             }
-                        },
-                    Image(systemName: "plus.rectangle.on.rectangle").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "arrowshape.turn.up.right").frame(width: 25, height: 35).foregroundColor(.white),
-                    Image(systemName: "ellipsis").frame(width: 25, height: 35).foregroundColor(.white),
-                    Spacer(),
-                    Image(systemName: "video.badge.ellipsis").frame(width: 25, height: 35).foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.fullScreen(.bottom2)) {[
-                    SeekBarWidget()
-                ]}
-                
-                controlService.configure(.fullScreen(.bottom3)) {[
-                    TimelineWidget(),
-                    Spacer(),
-                    Image(systemName: "arrow.down.right.and.arrow.up.left").foregroundColor(.white),
-                ]}
-                
-                controlService.configure(.fullScreen(.center)) { views in
-                    HStack(spacing: 30) {
-                        ForEach(views) { $0 }
+                        }
+                        
+                        controlService.configure(.halfScreen(.center)) {[
+                            StepBackWidget(),
+                            PlaybackWidget(),
+                            StepForwardWidget(),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.top1)) {[
+                            Text("Michael Jackson - Thriller (Official 4K Video)").lineLimit(1).foregroundColor(.white),
+                            Spacer(),
+                            Toggle("", isOn: $toggle).fixedSize().frame(width: 50),
+                            Image(systemName: "airplayvideo").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "captions.bubble").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "gearshape").frame(width: 25, height: 35).foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.bottom1)) {[
+                            Image(systemName: "hand.thumbsup").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "hand.thumbsdown").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "ellipsis.message")
+                                .frame(width: 25, height: 35).foregroundColor(.white)
+                                .allowsHitTesting(true)
+                                .onTapGesture {
+                                    context[FeatureService.self].present(.right(.squeeze(0))) {
+                                        AnyView(CommentWidget())
+                                    }
+                                },
+                            Image(systemName: "plus.rectangle.on.rectangle").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "arrowshape.turn.up.right").frame(width: 25, height: 35).foregroundColor(.white),
+                            Image(systemName: "ellipsis").frame(width: 25, height: 35).foregroundColor(.white),
+                            Spacer(),
+                            Image(systemName: "video.badge.ellipsis").frame(width: 25, height: 35).foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.bottom2)) {[
+                            SeekBarWidget()
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.bottom3)) {[
+                            TimelineWidget(),
+                            Spacer(),
+                            Image(systemName: "arrow.down.right.and.arrow.up.left").foregroundColor(.white),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.center)) { views in
+                            HStack(spacing: 30) {
+                                ForEach(views) { $0 }
+                            }
+                        }
+                        
+                        controlService.configure(.fullScreen(.center)) {[
+                            StepBackWidget(),
+                            PlaybackWidget(),
+                            StepForwardWidget(),
+                        ]}
+                        
+                        controlService.configure(.fullScreen(.top), transition: .opacity)
+                        controlService.configure(.fullScreen(.bottom), transition: .opacity)
+                        
+                        controlService.configure(displayStyle: .manual(firstAppear: true, animation: .default))
+                        
+                        controlService.configure(.fullScreen, insets: .init(top: 20, leading: 60, bottom: 34, trailing: 60))
+                        
+                        let player = context[RenderService.self].player
+                        let item = AVPlayerItem(url: Bundle.main.url(forResource: "demo", withExtension: "mp4")!)
+                        player.replaceCurrentItem(with: item)
+                        player.play()
                     }
-                }
-                
-                controlService.configure(.fullScreen(.center)) {[
-                    StepBackWidget(),
-                    PlaybackWidget(),
-                    StepForwardWidget(),
-                ]}
-                
-                controlService.configure(.fullScreen(.top), transition: .opacity)
-                controlService.configure(.fullScreen(.bottom), transition: .opacity)
-                
-                controlService.configure(displayStyle: .manual(firstAppear: true, animation: .default))
-                
-                controlService.configure(.fullScreen, insets: .init(top: 20, leading: 60, bottom: 34, trailing: 60))
-                
-                let player = context[RenderService.self].player
-                let item = AVPlayerItem(url: Bundle.main.url(forResource: "demo", withExtension: "mp4")!)
-                player.replaceCurrentItem(with: item)
-                player.play()
             }
+            Spacer()
+        }
+        .ignoresSafeArea(.all)
     }
 }
 
